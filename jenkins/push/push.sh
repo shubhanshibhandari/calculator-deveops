1dc0901ac2c319bacf37aaa2d2e0c074655b54ed
shubhanshibhandari <shubhanshibhandari111@gmail.com>
2023-03-14 23:04:13 +0530

    5th commit

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -9,7 +9,7 @@ echo "********************"
 echo "** Logging in ***"
 echo $PASS | docker login -u $USER --password-stdin
 echo "*** Tagging image ***"
-docker tag $IMAGE:$BUILD_TAG $USER/$IMAGE:$BUILD_NUMBER
+docker tag $IMAGE:$BUILD_NUMBER $USER/$IMAGE:$BUILD_NUMBER
 echo "*** Pushing image ***"
 docker push $USER/$IMAGE:$BUILD_NUMBER
 docker rmi  $USER/$IMAGE:$BUILD_NUMBER
