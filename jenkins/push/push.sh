810ef22e03c3bde131dce771ddec6bca51827960
shubhanshibhandari <shubhanshibhandari111@gmail.com>
2023-03-22 19:39:39 +0530

    deleting previous image

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -13,4 +13,5 @@ docker tag $IMAGE:$BUILD_NUMBER $USER/$IMAGE:$BUILD_NUMBER
 echo "*** Pushing image ***"
 docker push $USER/$IMAGE:$BUILD_NUMBER
 docker rmi  $IMAGE:$BUILD_NUMBER
+docker rmi  $IMAGE:$PRE_BUILD
 docker rmi  $USER/$IMAGE:$BUILD_NUMBER
